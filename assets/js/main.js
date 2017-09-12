dd8740af2b57522dedfc4501ae9e18d47c058146
Mariley20 <mariley2018@gmail.com>
2017-09-12 14:52:26 -0500

    numero de celular

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*Registrate*/
+$("#phone").intlTelInput({
+      // allowDropdown: false,
+      //autoHideDialCode: false,
+      //autoPlaceholder: "off",
+      // dropdownContainer: "body",
+      // excludeCountries: ["us"],
+      // formatOnDisplay: false,
+      // geoIpLookup: function(callback) {
+      //   $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+      //     var countryCode = (resp && resp.country) ? resp.country : "";
+      //     callback(countryCode);
+      //   });
+      // },
+      // hiddenInput: "full_number",
+      // initialCountry: "auto",
+      nationalMode: false,
+      onlyCountries: ['pe', 'mx', 'cl'],
+      placeholderNumberType: "MOBILE",
+      // preferredCountries: ['cn', 'jp'],
+       //separateDialCode: true,
+       preventInvalidDialCodes: true,
+      utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.0.3/js/utils.js"
+    });
