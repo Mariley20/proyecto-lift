413251b586dcd82756206986c0047cb56af6ffdd
Mariley20 <mariley2018@gmail.com>
2017-09-13 05:53:12 -0500

    subiendo archivo registraTelefono.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,24 @@
 /*Registrate*/
-$("#phone").intlTelInput({
-      // allowDropdown: false,
-      //autoHideDialCode: false,
-      //autoPlaceholder: "off",
-      // dropdownContainer: "body",
-      // excludeCountries: ["us"],
-      // formatOnDisplay: false,
-      // geoIpLookup: function(callback) {
-      //   $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-      //     var countryCode = (resp && resp.country) ? resp.country : "";
-      //     callback(countryCode);
-      //   });
-      // },
-      // hiddenInput: "full_number",
-      initialCountry: "auto",
-      geoIpLookup: function(callback) {
-            $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
-              var countryCode = (resp && resp.country) ? resp.country : "";
-              callback(countryCode);
-            });
-          },
-      nationalMode: false,
-      onlyCountries: ['pe', 'mx', 'cl'],
-      placeholderNumberType: "MOBILE",
-      // preferredCountries: ['cn', 'jp'],
-       //separateDialCode: true,
-       preventInvalidDialCodes: true,
-      utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.0.3/js/utils.js"
+let telInput = $("#phone");
+telInput.intlTelInput({
+  initialCountry: "auto",
+  geoIpLookup: function (callback) {
+    $.get('http://ipinfo.io', function () {}, "jsonp").always(function (resp) {
+      var countryCode = (resp && resp.country) ? resp.country : "";
+      callback(countryCode);
     });
-
-$("#probar").click(() => {
-	console.log($("#phone").intlTelInput("_getDialCode"));
-	}
-	);
+  },
+  nationalMode: false,
+  onlyCountries: ['pe', 'mx', 'cl'],
+  placeholderNumberType: "MOBILE",
+  preventInvalidDialCodes: true,
+  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.0.3/js/utils.js"
+});
+validarTelefono = () => {
+  if (telInput.intlTelInput("isValidNumber")) {
+    $('#siguiente').removeAttr('disabled');
+  } else {
+    $("#siguiente").attr('disabled', 'disabled');
+  }
+}
+telInput.on("keyup", validarTelefono);
