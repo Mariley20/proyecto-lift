fecaaeaa31331480f9678bb0a5982c8afcef6cb4
Mariley20 <mariley2018@gmail.com>
2017-09-13 08:04:38 -0500

    modificaciones

--- assets/js/validarDatos.js
+++ assets/js/validarDatos.js
@@ -7,12 +7,11 @@ $(document).ready(function () {
 
     function validarCorreo(){
         //console.log(emailValido.test($(this).val()));
-        if (emailValido.test($(this).val())) {  
-            console.log("entro") 
-            $("#spanCorreo").attr('class', 'glyphicon glyphicon-ok form-control-feedback')
+        if (emailValido.test($(this).val())) {
+            $("#spanCorreo i").attr('class', 'glyphicon glyphicon-ok form-control-feedback')
         }else{
             console.log('entro')
-            $("#spanCorreo").attr('class', 'glyphicon glyphicon-remove form-control-feedback')
+            $("#spanCorreo i").attr('class', 'glyphicon glyphicon-remove form-control-feedback')
         }
     }
     function validarNombreApellido() {
@@ -23,25 +22,3 @@ $(document).ready(function () {
         }
     }
 });
-
-/*
-        
-    $(".boton").click(function () {        
-        $(".error").remove();        
-        if ($(".nombre").val() == "") {            
-            $(".nombre").focus().after("<span class='error'>Ingrese su nombre</span>");            
-            return false;        
-        } else if ($(".email").val() == "" || !emailValido.test($(".email").val())) {            
-            $(".email").focus().after("<span class='error'>Ingrese un email correcto</span>");            
-            return false;        
-        }    
-    });    
-    $(".nombre, .apellidos").keyup(function () {        
-        if ($(this).val() != "") {            
-            $(".error").fadeOut();            
-            return false;        
-        }    
-    });    
-    $(".email").keyup(function () {            });
-});
-*/
